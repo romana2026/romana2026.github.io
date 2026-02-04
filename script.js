95baf26d87cd652e76611c6b0e9fe72ed0794480
romana2026 <mihail.ursu2018@gmail.com>
2026-02-04 15:34:52 +0200

    Update API endpoint in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ﻿let token = null;
-fetch('https://openai-server-4d3w.onrender.com/checka',
+fetch('https://openai-server-853z.onrender.com/checka',
 {
  method: 'POST',
  headers: { 'Content-Type': 'application/json' },
@@ -45,7 +45,7 @@ const requestFunc = () => {
   sendButton.innerText = 'O clipă...';
   let message = { "role": "user", "content": inp.value };
   conversation.push(message);
-  axios.post('https://openai-server-4d3w.onrender.com/api/chat',
+  axios.post('https://openai-server-/853z.onrender.com/api/chat',
   {
    messages: conversation,
    token: token
@@ -62,5 +62,6 @@ const requestFunc = () => {
   });
  }
 }
+
 
 
